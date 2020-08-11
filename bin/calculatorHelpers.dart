439c5b8870a230d283e7d0acb4b2b58fba009ac1
Yusuf Özgül <yozgul@icloud.com>
2020-08-11 16:37:24 +0300

    added calculator helpers

---
+++ bin/calculatorHelpers.dart
@@ -0,0 +1,13 @@
+enum CalculatorOperands { plus, minus, divided, multiply }
+
+class CalculatorHelp {
+  helper() {
+    print("Calculator Usage: dart bin/main.dart [num] [sign] [num]\n");
+    print("Signs [+]  [-]  [*]  [/]");
+    print("\n\n");
+    print("sample: dart bin/main.dart 12 + 15");
+    print("");
+    print("=_________________");
+    print("\t27");
+  }
+}
